de4eaffb57c8e640ca29f8e770deda3a1f597e30
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-10-10 00:04:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# conan-cista
+
+## Package Status
+
+| Windows (Visual Studio) | Linux | macOS |
+|:-----------------------:|:-----:|:-----:|
+|[![Build status](https://github.com/SpaceIm/conan-cista/workflows/.github/workflows/windows.yml/badge.svg?branch=testing%2F0.7)](https://github.com/SpaceIm/conan-cista/actions/workflows/windows.yml?query=branch%3Atesting%2F0.7)|[![Build status](https://github.com/SpaceIm/conan-cista/workflows/.github/workflows/linux.yml/badge.svg?branch=testing%2F0.7)](https://github.com/SpaceIm/conan-cista/actions/workflows/linux.yml?query=branch%3Atesting%2F0.7)|[![Build status](https://github.com/SpaceIm/conan-cista/workflows/.github/workflows/macos.yml/badge.svg?branch=testing%2F0.7)](https://github.com/SpaceIm/conan-cista/actions/workflows/macos.yml?query=branch%3Atesting%2F0.7)|
+
+## Description
+
+[Conan.io](https://conan.io) package for [Cista++](https://github.com/felixguendling/cista).
---
+++ conanfile.py
@@ -0,0 +1,33 @@
+from conans import ConanFile, tools
+import os
+
+
+class CistaConan(ConanFile):
+    name = "cista"
+    description = (
+        "Cista++ is a simple, open source (MIT license) C++17"
+        "compatible way of (de-)serializing C++ data structures."
+    )
+    license = "MIT"
+    topics = ("cista", "serialization", "deserialization", "reflection")
+    homepage = "https://github.com/felixguendling/cista"
+    url = "https://github.com/conan-io/conan-center-index"
+    settings = "compiler"
+    no_copy_source = True
+
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 17)
+
+    def source(self):
+        for file in self.conan_data["sources"][self.version]:
+            filename = os.path.basename(file["url"])
+            tools.download(filename=filename, **file)
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses")
+        self.copy("cista.h", dst="include")
+
+    def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "cista"
+        self.cpp_info.names["cmake_find_package_multi"] = "cista"
